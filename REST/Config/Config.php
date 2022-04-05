26355da65a2d492f6484cab6c09904c1521f5ac4
Sebastian Betancourt <jsebas.95@hotmail.com>
2022-04-05 17:11:02 -0500

    first commit

---
+++ REST/Config/Config.php
@@ -0,0 +1,13 @@
+<?php 
+
+const Base_url = "http://localhost/CIC/REST";
+
+//Datos de conexión a base de datos
+const DB_HOST = "localhost"; //servidor de la DB
+const DB_NAME = "db_cursos"; //nombre DB
+const DB_USER = "root"; //usuario DB
+const DB_PASSWORD = ""; //cantraseña DB
+const DB_CHARSET = "charset=utf8"; //codificación
+
+
+?>
---
+++ REST/Libraries/Core/Autoload.php
@@ -0,0 +1,10 @@
+<?php
+
+    // Autoload, recibe el controlador e instancia su clase para pasrala como parametro para requerirla
+    spl_autoload_register(function($class){
+        if (file_exists("Libraries/".'Core/'.$class.".php")) {
+            require_once("Libraries/".'Core/'.$class.".php");
+        }
+    });
+    
+?>
